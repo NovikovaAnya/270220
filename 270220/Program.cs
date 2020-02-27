e5fa66b3367f1aa4c5fb3efc68e933bb86238df0
Anna Novikova <meoowway@gmail.com>
2020-02-27 16:02:07 +0300

    Добавьте файлы проекта.

---
+++ 270220/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Math;
+
+namespace _270220
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            double a;
+            double b;
+            double c;
+            double xmin;
+            double xmax;
+            Console.WriteLine("Введите a, введите b, введите c");
+            string d = Console.ReadLine();
+            a = double.Parse(d);
+            d = Console.ReadLine();
+            b = double.Parse(d);
+            d = Console.ReadLine();
+            c = double.Parse(d);
+            Console.WriteLine("Введите границы области");
+            string f = Console.ReadLine();
+            xmin = double.Parse(f);
+            string g = Console.ReadLine();
+            xmax = double.Parse(g);
+
+            double x;
+            
+
+
+           
+
+
+
+
+        }
+    }
+}
